a9477a1516a50b5558c13dc843d38cb9ed9e6f97
jedong <jedong@10.187.28.244>
2019-05-10 13:45:45 +0800

    multiple profile for package

--- src/main/java/com/bmw/dwh/Application.java
+++ src/main/java/com/bmw/dwh/Application.java
@@ -3,12 +3,19 @@ package com.bmw.dwh;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.bmw.dwh.mapper")
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 }
